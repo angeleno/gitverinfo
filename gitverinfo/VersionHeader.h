23fa8f33bbe5cb7d434ef2ec4c0e42cf5ca4200a
angeleno <angeleno@nullptr.me>
2018-03-12 20:06:50 -0700

    last commit

--- gitverinfo/VersionHeader.h
+++ gitverinfo/VersionHeader.h
@@ -2,5 +2,5 @@
 #ifndef VersionHeader_H_Included     
 #define VersionHeader_H_Included     
 #define GIT_BRANCH "master"        
-#define GIT_COMMIT_HASH "88e299d837cba8fee8344d4f93fa4ec20434ed1e"   
+#define GIT_COMMIT_HASH "a365bd01c75255b11c81eb7333d2bc4c5f1c08f7"   
 #endif                               
